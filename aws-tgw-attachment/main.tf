f96ef49460cb7d001e22748535e21a93c34730e7
Bryan B <bbartik78@gmail.com>
2021-11-26 14:48:09 -0800

    updates

--- aws-tgw-attachment/main.tf
+++ aws-tgw-attachment/main.tf
@@ -38,7 +38,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
 }
 
 resource "aws_ec2_transit_gateway_route" "example" {
-  destination_cidr_block         = data.aws_vpc.cidr_block
+  destination_cidr_block         = data.aws_vpc.this.cidr_block
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.this.id
   transit_gateway_route_table_id = data.aws_ec2_transit_gateway.this.id
 }
